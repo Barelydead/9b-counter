a6f04680074c81cf48c262fef0c80b5c9a5e054e
Christofer Jungberg <kiffen_@hotmail.com>
2019-08-30 20:12:10 +0200

    scaffold basic auth

---
+++ resources/views/partials/intro.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+  <div class="row">
+    <div class="intro">
+      <h1 class="intro__title">{{ $title }}</h1>
+      <p class="intro__preamble">{{ $preamble }}</p>
+    </div>
+  </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ Route::get('/climbers', 'ClimberController@index');
 Route::get('/climbers/{climber}', 'ClimberController@show');
 
 Route::get('/routes', 'RouteController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
