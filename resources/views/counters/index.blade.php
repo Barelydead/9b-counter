7bcca1280b29e32f1b395f9a6ce151fa7b549859
Christofer Jungberg <kiffen_@hotmail.com>
2020-03-30 18:27:12 +0200

    featured climbers on start

--- resources/views/counters/index.blade.php
+++ resources/views/counters/index.blade.php
@@ -1,15 +1,18 @@
 
 @extends('layouts.public')
 
-@section('title', 'Climbers')
+@section('title', 'Home')
 
 @section('main')
   @include('components/intro', ['title' => '9b Counter digital', 'preamble' => 'Browse all the climbers that are lucky and strong enough to be placed on the 9b counter (and all the other counters).'])
 
   <div class="container mb-5">
+    <div class="row mt-5 mb-2">
+      <h2 class="col-12">Counters</h2>
+    </div>
     @foreach ($counterGroups as $key => $counters)
-      <div class="row mt-5 mb-2">
-        <h2 class="col-12 text-md-center">{{ $counters->first()->year }}</h2>
+      <div class="row mb-2">
+        <h3 class="col-12">{{ $counters->first()->year }}</h3>
       </div>
       <div class="row">
         @foreach ($counters as $key => $counter)
@@ -24,5 +27,17 @@
         @endforeach
       </div>
     @endforeach
+
+    <div class="row mt-5 mb-2">
+      <h2 class="col-12">Climbers</h2>
+      <a class="blurb__link col-sm-12 d-block" href="{{ route('climbers.index') }}">
+        <article class="blurb blurb--primary">
+          <div class="blurb__inner">
+          <small class="blurb__category text-uppercase"></small>
+            <h2 class="blurb__header">Featured climbers</h2>
+          </div>
+        </article>
+      </a>
+    </div>
   </div>
 @endsection
